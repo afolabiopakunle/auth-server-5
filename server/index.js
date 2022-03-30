99a1a704709569ef488a0499397f596d59907ace
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-03-30 08:42:19 +0100

    postman works drops data into db successfully

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 const api = require('./routes/api');
+
+const db = 'mongodb+srv://afolabi:8899fifafa@cluster0.bwrba.mongodb.net/eventsdb?retryWrites=true&w=majority';
+
+mongoose.connect(db, (err) => err ? console.log(err) : console.log('Connected to mongoDB successfully'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the home page');
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const User = require('../models/user');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Welcome to the apis home page')
 })
 
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((err, registeredUser) => err ? console.log(err): console.log(registeredUser) );
+})
 
 module.exports = router;
