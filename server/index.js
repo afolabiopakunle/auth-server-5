e0a609afda12b568a6a8d349f8d51699daeb45b1
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-03-30 08:14:26 +0100

    api route works

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+const api = require('./routes/api');
 
 app.get('/', (req, res) => {
     res.send('Welcome to the home page');
 })
+
+app.use('/api', api);
 
 const PORT = 3000;
 
